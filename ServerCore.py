e8ad984bd63392d05abf7d090928ccb7e25fe741
GenPage <genpage@technicpack.net>
2014-06-23 15:29:07 -0400

    Update to v0.3-alpha See changelog.txt for details

--- ServerCore.py
+++ ServerCore.py
@@ -17,6 +17,8 @@ from progressbar import ProgressBar, Percentage, Bar, RotatingMarker, ETA, FileT
 def main():
 
     global args
+    global zipsOutput
+    zipsOutput = '~/TechnicServerCore/servers/'
 
     if args.verbose: print sys.argv
     if args.listPacks:
@@ -27,29 +29,38 @@ def main():
             else:
                 listPacks()
         else:
-	    if len(sys.argv) > 2:	    
-	        parser.error(" Too many args")
-                sys.exit(2)     
-	    else:
- 	        listPacks()
+            if len(sys.argv) > 2:
+                    parser.error(" Too many args")
+                    sys.exit(2)
+            else:
+                listPacks()
     elif args.modpack:
         if not args.verbose:
-	    if len(sys.argv) == 2:
-	        displayPack(args.modpack)
-	    elif len(sys.argv) == 4:
-	        if args.install:
-	            sys.stdout.write("Install directory: [" + os.path.expanduser('~/TechnicServerCore/servers/') + "] ")
+            if len(sys.argv) == 2:
+                displayPack(args.modpack)
+            elif len(sys.argv) == 3:
+                if args.download:
+                    if 'all' in args.modpack:
+                        downloadAllPacks(args.download)
+                    else:
+                        downloadPack(args.modpack, args.download)
+            elif len(sys.argv) == 4:
+                if args.install:
+                    sys.stdout.write("Install directory: [" + os.path.expanduser(zipsOutput) + "] ")
                     choice = raw_input()
                     if not choice:
-                        choice = os.path.expanduser('~/TechnicServerCore/servers/')
+                        choice = os.path.expanduser(zipsOutput)
                     installPack(args.modpack, args.install, os.path.expanduser(choice))
                 elif args.download:
-	            downloadPack(args.modpack, args.download)
+                    if 'all' in args.modpack:
+                        downloadAllPacks(args.download)
+                    else:
+                        downloadPack(args.modpack, args.download)
                 elif args.wipe:
-                    sys.stdout.write("Install directory: [" + os.path.expanduser('~/TechnicServerCore/servers/') + "] ")
+                    sys.stdout.write("Install directory: [" + os.path.expanduser(zipsOutput) + "] ")
                     choice = raw_input()
                     if not choice:
-                        choice = os.path.expanduser('~/TechnicServerCore/servers/')
+                        choice = os.path.expanduser(zipsOutput)
                     wipePack(args.modpack, args.wipe, os.path.expanduser(choice))
             elif len(sys.argv) > 4:
                 parser.error("Too many args provided")
@@ -57,20 +68,29 @@ def main():
         elif args.verbose:
             if len(sys.argv) == 3:
                 displayPack(args.modpack)
+            elif len(sys.argv) == 4:
+                if args.download:
+                    if 'all' in args.modpack:
+                        downloadAllPacks(args.download)
+                    else:
+                        downloadPack(args.modpack, args.download)
             elif len(sys.argv) == 5:
                 if args.install:
-                    sys.stdout.write("Install directory: [" + os.path.expanduser('~/TechnicServerCore/servers/') + "] ")
+                    sys.stdout.write("Install directory: [" + os.path.expanduser(zipsOutput) + "] ")
                     choice = raw_input()
                     if not choice:
-                        choice = os.path.expanduser('~/TechnicServerCore/servers/')
+                        choice = os.path.expanduser(zipsOutput)
                     installPack(args.modpack, args.install, os.path.expanduser(choice))
                 elif args.download:
-                    downloadPack(args.modpack, args.download)
+                    if 'all' in args.modpack:
+                        downloadAllPacks(args.download)
+                    else:
+                        downloadPack(args.modpack, args.download)
                 elif args.wipe:
-                    sys.stdout.write("Install directory: [" + os.path.expanduser('~/TechnicServerCore/servers/') + "] ")
+                    sys.stdout.write("Install directory: [" + os.path.expanduser(zipsOutput) + "] ")
                     choice = raw_input()
                     if not choice:
-                        choice = os.path.expanduser('~/TechnicServerCore/servers/')
+                        choice = os.path.expanduser(zipsOutput)
                     wipePack(args.modpack, args.wipe, os.path.expanduser(choice))
             elif len(sys.argv) > 5:
                 parser.error("Too many args provided")
@@ -87,7 +107,7 @@ def getBuild(build):
     elif 'recommended' in build:
         return str(currentPack.recommended)
     else:
-        return build
+        return str(build)
 
 def getPacks():
 
@@ -114,10 +134,24 @@ def displayPack(pack):
     getPackInfo(pack)
     currentPack.printModpack()
 
+def downloadAllPacks(build):
+
+    print 'Downloading all available packs...'
+    for key in availPacks:
+        if ('latest' in build):
+            downloadPack(key, 'latest')
+        elif 'recommended' in build:
+            downloadPack(key, 'recommended')
+        else:
+            downloadPack(key, build)
+
+    print 'Downloading Complete.'
+
 def downloadPack(pack, build):
 
     getPackInfo(pack)
     currentBuild = getBuild(build)
+    packAvailable = True
     if pack == 'attack-of-the-bteam':
         url = "http://mirror.technicpack.net/Technic/servers/bteam/BTeam_Server_v" + currentBuild + ".zip"
     elif pack == 'tekkitmain':
@@ -133,28 +167,34 @@ def downloadPack(pack, build):
     elif pack == 'voltz':
         url = "http://mirror.technicpack.net/Technic/servers/voltz/Voltz_Server_v" + currentBuild + ".zip"
     else:
-        print currentPack.name + "has been retired from the Technic Launcher and is no longer available."
-        sys.exit(0)
+        print currentPack.name + " has been retired from the Technic Launcher and is no longer available."
+        packAvailable = False
     
-    if args.verbose: print url
-    print "\n\rDownlading build: " + currentBuild + " of " + currentPack.name + "\n\r"
-    serverDir = os.path.expanduser('~/TechnicServerCore/serverZips/' + currentPack.display_name)
-    if args.verbose: print serverDir
-    serverFile = serverDir + '/' + currentPack.display_name + '-v' + currentBuild + '.zip'
-    if args.verbose: print serverFile
-    if not os.path.exists(serverDir):
-        print "Creating directory: " + serverDir
-        os.makedirs(serverDir)
 
-    if os.path.exists(serverFile):
-        if confirmInput("Zip already downloaded. Do you wish to re-download?", "yes"):  
-            silentRemove(serverFile)
-            downloadFile(url, serverFile)
+    if packAvailable == True:
+        if currentBuild in currentPack.builds:
+            if args.verbose: print url
+            print "\n\rDownlading build: " + currentBuild + " of " + currentPack.name + "\n\r"
+            serverDir = os.path.expanduser('~/TechnicServerCore/serverZips/' + currentPack.display_name)
+            if args.verbose: print serverDir
+            serverFile = serverDir + '/' + currentPack.display_name + '-v' + currentBuild + '.zip'
+            if args.verbose: print serverFile
+            if not os.path.exists(serverDir):
+                print "Creating directory: " + serverDir
+                os.makedirs(serverDir)
+
+            if os.path.exists(serverFile):
+                if confirmInput("Zip already downloaded. Do you wish to re-download?", "no"):
+                    silentRemove(serverFile)
+                    downloadFile(url, serverFile)
+                else:
+                    print "Download aborted! \n\r"
+            else:
+                downloadFile(url, serverFile)
+
+            return serverFile
         else:
-            print "Download aborted! \n\r"
-    else:
-        downloadFile(url, serverFile)
-    return serverFile
+            print "Build " + currentBuild + " does not exist for " + currentPack.name
 
 def installPack(pack, build, dest):
 
@@ -204,6 +244,8 @@ def listPacks():
     print "==========="
     for key in availPacks:
         print '{0} ==> {1}'.format(availPacks[key], key)
+    print
+    print "\n\rMirror URL: " + mirrorURL
     print
 
 def silentRemove(file):
@@ -261,18 +303,18 @@ if __name__ == '__main__':
         start_time = time.time()
         parser = argparse.ArgumentParser(description=globals()['__doc__'])
         parser.add_argument ('-ls', '--listPacks', action='store_true', default=False, help='list all available modpacks')
-	parser.add_argument ('--install', action='store', nargs=1, metavar=('<build>'), help='installs the provided server build')	
-	parser.add_argument ('--download', action='store', nargs=1, metavar=('<build>'), help='downloads the provided server build')
-	parser.add_argument ('--wipe', action='store', nargs=1, metavar=('<build>'), help='wipe and install the provided server build')
+        parser.add_argument ('--install', const='recommended', nargs='?', metavar=('<build>'), help='installs the provided server build')	
+        parser.add_argument ('--download', const='recommended', nargs='?', metavar=('<build>'), help='downloads the provided server build')
+        parser.add_argument ('--wipe', const='recommended', nargs='?', metavar=('<build>'), help='wipe and install the provided server build')
         parser.add_argument ('modpack', nargs='?',  action='store', help='determines which pack to use')
         parser.add_argument ('-v', '--version', action='version', version=globals()['__doc__'])
-	parser.add_argument ('--verbose', action='store_true', default=False, help='verbose output')
+        parser.add_argument ('--verbose', action='store_true', default=False, help='verbose output')
         args = parser.parse_args()
         if not len(sys.argv) > 1:
             parser.error ('Missing argument')
         if args.verbose: print time.asctime()
-	getPacks()
-	main()
+        getPacks()
+        main()
         if args.verbose: print time.asctime()
         if args.verbose: print 'TOTAL TIME IN SECONDS:',
         if args.verbose: print (time.time() - start_time)
@@ -282,8 +324,8 @@ if __name__ == '__main__':
     except SystemExit, e: # sys.exit()
         raise e
     except urllib2.HTTPError, e: # no json found
-	print 'Error: No JSON found'
-	sys.exit(4)
+        print 'Error: No JSON found'
+        sys.exit(4)
     except Exception, e:
         print 'ERROR, UNEXPECTED EXCEPTION'
         print str(e)
