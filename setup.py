681daadb7e760d47187951f7e081b46e7c429fd9
GenPage <genpage@technicpack.net>
2014-06-25 10:16:24 -0400

    Refractor for PyPi Package

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='TechnicServerCore',
+    version='0.3.0-alpha',
+    author='Dylan Page',
+    maintainer='Dylan Page',
+    author_email='genpage@technicpack.net',
+    maintainer_email='genpage@technicpack.net',	
+    packages=['servercore', 'servercore.test'],
+
+    url='http://github.com/GenPage/ServerCore/',
+    license='LICENSE.txt',
+    description='Custom wrapper that downloads and updates Technic modpacks specifically for servers.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "progressbar >= 2.2",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Terminals'
+    ],
+)
